90b5c2a7378833c656ee5f009a9ec0064bef5acb
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-08-12 15:10:42 +0200

    added button, paragraph, header components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 import Htag from "./Components/Htag/Htag";
-
+import Button from "./Components/Button/Button";
+import P from "./Components/P/P";
 export default function Home(): JSX.Element {
 
   return (
@@ -9,6 +10,14 @@ export default function Home(): JSX.Element {
     <Htag tag='h1'>Текст</Htag>
     <Htag tag='h2'>Текст2</Htag>
     <Htag tag='h3'>Текст3</Htag>
+    <Button appearance="primary"> Кнопка праймари</Button>
+    <Button appearance="primary" arrow="right"> Кнопка праймари</Button>
+    <Button appearance="ghost" arrow="down"> Кнопка праймари</Button>
+    <Button appearance="ghost"> Кнопка </Button>
+    <P>Та хуйлан</P>
+    <P size="s">Та хуйлан</P>
+    <P size={'l'}>Та хуйлан</P>
+
 
     </>
   );
